4f0c7c20c75bcaee82585b1e238e547a68fc37fc
unknown <tuj57607@temple.edu>
2023-12-20 22:13:44 -0500

    fixing deployment

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
